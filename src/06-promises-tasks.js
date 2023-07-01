19319c1848f99ecf52e6c2629627a299feeb294b
Tedzury <asdrogachev@gmail.com>
2023-07-01 23:55:07 +0300

    feat: objects section - done, except last task

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,12 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === undefined) reject(new Error('Error: Wrong parameter is passed! Ask her again.'));
+    if (isPositiveAnswer) resolve('Hooray!!! She said "Yes"!');
+    if (!isPositiveAnswer) resolve('Oh no, she said "No".');
+  });
 }
 
 
